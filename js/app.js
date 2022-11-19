f39da8e76f9c460f730e158d483ba66a15455469
Gabriel Prato <pratogabriel@outlook.com>
2022-11-18 23:08:09 -0400

    First Commit

---
+++ js/app.js
@@ -0,0 +1,139 @@
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+const paginacionDIV = document.querySelector('#paginacion');
+
+const registrosPorPagina = 40;
+let iterador;
+let totalPaginas;
+let paginaActual = 1;
+
+window.onload = () =>{
+    formulario.addEventListener('submit', validarFormulario);
+}
+
+function validarFormulario(e){
+    e.preventDefault();
+
+    const terminoBusqueda = document.querySelector('#termino').value;
+
+    if(terminoBusqueda===''){
+        mostrarAlerta('Agrega un termino de bsuqueda');
+        return;
+    }
+
+    buscarImagenes();
+}
+
+function mostrarAlerta(msg){
+
+    const existeAlerta = document.querySelector('.bg-red-100');
+
+    if(!existeAlerta){
+        const alerta = document.createElement('p')
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+        alerta.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block sm:inline">${msg}</span>
+        `;
+    
+        formulario.appendChild(alerta);
+    
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
+}
+
+function buscarImagenes(){
+    
+    const termino = document.querySelector('#termino').value;
+
+    const key = '30559724-0e622efe28834ff59870933c5';
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado =>{
+            // console.log(resultado)
+            totalPaginas = calcularPag(resultado.totalHits);
+            console.log(totalPaginas)
+            mostrarImagenes(resultado.hits)
+        })
+}
+
+//Generador que regista la cantidad de elementos de las paginas
+
+function *crearPaginador(total){
+    for (let i = 1; i <= total; i++){
+        yield i;
+    }
+}
+
+function calcularPag(total){
+    return parseInt(Math.ceil(total/registrosPorPagina));
+}
+
+function mostrarImagenes(imagenes){
+    // console.log(imagenes);
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // Iterar sobre el arreglo de imagenes y construir el HTML
+
+    imagenes.forEach(imagen => {
+
+        const{previewURL, likes, views, largeImageURL} = imagen;
+
+        resultado.innerHTML +=`
+        <div class="w-1/2 md:w-1/3 lg:w1/4 p-3 mb-4">
+            <div class="bg-white">
+                <img class="w-full" src="${previewURL}">
+
+                <div class="p-4">
+                    <p class="font-bold">${likes}<span class="font-light"> Me Gusta</span></p>
+                    <p class="font-bold">${views}<span class="font-light"> Veces vista</span></p>
+
+                    <a class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                        href="${largeImageURL}" target="_blank" rel="noopener noreferrer"
+                    >
+                        Abrir Imagen
+                    </a>
+                </div>
+            </div>        
+        </div>
+        `;
+    });
+
+    //limpiar el paginador previo
+
+    while(paginacionDIV.firstChild){
+        paginacionDIV.removeChild(paginacionDIV.firstChild)
+    }
+
+    //Generar el nuevo HTMLs
+    imprimirPaginador();
+}
+
+function imprimirPaginador(){
+    iterador = crearPaginador(totalPaginas);
+
+    while(true){
+        const {value, done} = iterador.next()
+        if(done) return;
+
+        //Caso contrario, genera un boton por cada elemento en el generador
+        const boton = document.createElement('a')
+        boton.href = '#';
+        boton.dataset.pagina = value;
+        boton.textContent = value;
+        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'rounded');
+
+        boton.onclick = ()=>{
+            paginaActual = value;
+            buscarImagenes();
+        }
+
+        paginacionDIV.appendChild(boton);
+    }
+}
